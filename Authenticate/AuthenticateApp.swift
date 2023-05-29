13b452e15975c5a5f7b5a3dc5dc604f10bf39acc
Mohan <mohan@hansaitechnosoft.com>
2023-05-29 16:44:10 +0530

    Initial Commit

---
+++ Authenticate/AuthenticateApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthenticateApp.swift
+//  Authenticate
+//
+//  Created by Diwakar Reddy  on 29/05/23.
+//
+
+import SwiftUI
+
+@main
+struct AuthenticateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
